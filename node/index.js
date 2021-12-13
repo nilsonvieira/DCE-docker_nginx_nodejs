3cf9442006247281a0392df18c46de65645396ed
Nilson R S Vieira <nilsonvieira@gmail.com>
2021-12-13 14:52:18 -0300

    Adjust: Conexão com o Banco funcional já criando as tabelas e inserindo dados.

--- node/index.js
+++ node/index.js
@@ -1,41 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
-var mysql = require('mysql');
+const express = require("express");
+const app = express();
+const port = 3000;
+var mysql = require("mysql");
 
 var con = mysql.createConnection({
   host: "ce-mysql",
   user: "root",
-  password: "root"
+  password: "root",
+  database: "nodedb",
+});
+const sqltable = "CREATE TABLE  people (name varchar(255))";
+con.query(sqltable);
+const sqlvalues = `INSERT INTO people(name) values('Nilson')`;
+con.query(sqlvalues);
+con.end();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Full Cycle Rocks</h1>");
 });
 
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    con.query("CREATE DATABASE nodedb", function (err, result) {
-      if (err) throw err;
-      console.log("Database created");
-    });
-  });
-
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    var sql = "create table  people (name varchar(255))";
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log("Table created");
-    });
-  });
-
-const sql = `INSERT INTO people(name) values('Nilson')`
-con.query(sql)
-con.end()
-
-app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle Rocks</h1>')
-})
-
-app.listen(port, ()=> {
-    console.log('Rodando na porta ' + port)
-})
+app.listen(port, () => {
+  console.log("Rodando na porta " + port);
+});
