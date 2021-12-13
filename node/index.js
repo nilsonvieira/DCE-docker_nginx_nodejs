24bbf75ded159db3c3a1d2c829bf504e715928b8
Nilson R S Vieira <nilsonvieira@gmail.com>
2021-12-13 10:29:37 -0300

    Adjust: Ainda com erro na comunicação com o Banco  - Container do node nao sobe

--- node/index.js
+++ node/index.js
@@ -1,6 +1,36 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "ce-mysql",
+  user: "root",
+  password: "root"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query("CREATE DATABASE nodedb", function (err, result) {
+      if (err) throw err;
+      console.log("Database created");
+    });
+  });
+
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "create table  people (name varchar(255))";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Table created");
+    });
+  });
+
+const sql = `INSERT INTO people(name) values('Nilson')`
+con.query(sql)
+con.end()
 
 app.get('/', (req,res) => {
     res.send('<h1>Full Cycle Rocks</h1>')
